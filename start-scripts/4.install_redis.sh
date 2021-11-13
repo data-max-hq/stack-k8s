3ac8734fb76689d2592c0dfb7e02af3e65b85e2f
Sadik Bakiu <sbakiu@gmail.com>
2021-11-13 19:41:45 +0100

    redis fix

--- start-scripts/4.install_redis.sh
+++ start-scripts/4.install_redis.sh
@@ -1,12 +1,13 @@
 #!/bin/zsh
 NAMESPACE=customer-ns
+NAME=redis-test
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
-helm install redis-test \
- --namespace $NAMESPACE
- --set persistence.storageClass=nfs-client,redis.replicas.persistence.storageClass=nfs-client bitnami/redis \
- --set volumePermissions.enabled=true
+helm install --namespace $NAMESPACE \
+ --set persistence.storageClass=nfs-client,redis.replicas.persistence.storageClass=nfs-client \
+ --set volumePermissions.enabled=true \
+ $NAME bitnami/redis
 
 export REDIS_PASSWORD=$(kubectl get secret --namespace $NAMESPACE redis-test -o jsonpath="{.data.redis-password}" | base64 --decode)
-kubectl run --namespace $NAMESPACE redis-client --restart='Never'  --env REDIS_PASSWORD=$REDIS_PASSWORD  --image docker.io/bitnami/redis:6.2.5-debian-10-r63 --command -- sleep infinity
-kubectl exec --tty -i redis-client --namespace $NAMESPACE -- bash
+#kubectl run --namespace $NAMESPACE redis-client --restart='Never'  --env REDIS_PASSWORD=$REDIS_PASSWORD  --image docker.io/bitnami/redis:6.2.5-debian-10-r63 --command -- sleep infinity
+#kubectl exec --tty -i redis-client --namespace $NAMESPACE -- bash

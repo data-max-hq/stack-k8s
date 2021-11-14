aa87cd6aaf92e9292e098169cfc33418c7709736
Sadik Bakiu <sbakiu@gmail.com>
2021-11-14 22:27:55 +0100

    graphql ambassador mapping

--- start-scripts/2.install_prefect.sh
+++ start-scripts/2.install_prefect.sh
@@ -4,8 +4,10 @@ helm repo update
 NAMESPACE=customer-ns
 NAME=prefect-server
 helm install --create-namespace --namespace $NAMESPACE  $NAME prefecthq/prefect-server \
-  --set jobs.createTenant.enabled=true && \
-  --set apollo.ingress.ingressClassName=ambassador && \
-  --set ui.ingress.ingressClassName=ambassador && \
+  --set jobs.createTenant.enabled=true \
+  --set apollo.ingress.ingressClassName=ambassador \
+  --set ui.ingress.ingressClassName=ambassador \
+  --set ui.apolloApiUrl=http://localhost:8080/graphql && \
 kubectl rollout status  -n $NAMESPACE deployment/prefect-server-ui -w && \
+kubectl rollout status  -n $NAMESPACE deployment/prefect-server-apollo -w && \
 kubectl rollout status  -n $NAMESPACE deployment/prefect-server-graphql -w
